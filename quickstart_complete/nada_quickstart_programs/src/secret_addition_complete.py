997403a7fb6ab3a46617150fa728923e447cac2f
RUPAM SADHUKHAN <115567972+GRUMPY-TUCKER@users.noreply.github.com>
2024-07-04 22:20:53 +0530

    Update secret_addition_complete.py

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,23 @@
 from nada_dsl import *
+
 def nada_main():
-
     party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
+    party3 = Party(name="Party3")
+    a = SecretInteger(Input(name="A", party=party1))
+    b = SecretInteger(Input(name="B", party=party2))
 
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    
+    result : list[SecretInteger] = [] 
+    result.append(b)
+    b= a%b
+    result.append(b)
 
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    new_int = my_int1 + my_int2
-
-    return [Output(new_int, "my_output", party1)]
+    outputs: list[Output] = []
+    for i in range(2):
+        outputs.append(Output(
+            result[i],
+            "result_" + str(i),
+            party3
+        ))
+    return outputs
